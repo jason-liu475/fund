37f744564e851ee34a1d560e6c4ffc5812c02727
liuhuan <79523503@qq.com>
2020-11-12 23:21:02 +0800

    修改获取实时基金数据时间

--- src/main/java/org/liu/fund/schedule/FundDataCollectionTask.java
+++ src/main/java/org/liu/fund/schedule/FundDataCollectionTask.java
@@ -39,7 +39,7 @@ public class FundDataCollectionTask {
 		}
 	}
 	//@Scheduled(fixedRate = 10 * 60 * 1000)
-	@Scheduled(cron = "0 30/45 9-13 0 0 1/5 ")
+	@Scheduled(cron = "0 30/45 9-13 0 0 1/5")
 	public void collectionAllFundData(){
 		int retryCount = 0;
 		SysTaskModel sysTaskModel = null;

2eaa67e415e3661461080645adb99d018f46a545
liuhuan <79523503@qq.com>
2020-11-12 22:35:44 +0800

    删除最近的暂停申购、赎回基金信息

--- src/main/java/org/liu/fund/service/impl/FundDataCollectionServiceImpl.java
+++ src/main/java/org/liu/fund/service/impl/FundDataCollectionServiceImpl.java
@@ -9,6 +9,8 @@ import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -48,6 +50,9 @@ public class FundDataCollectionServiceImpl implements FundDataCollectionService 
 	private FundInfoService fundInfoService;
 	@Autowired
 	private FundRealTimeDataService fundRealTimeDataService;
+	@Autowired
+	private FundSuspendService fundSuspendService;
+
 	@Value("${system.http.fund.host}")
 	private String host;
 	@Value("${system.http.fund.fundCompany}")
@@ -56,7 +61,14 @@ public class FundDataCollectionServiceImpl implements FundDataCollectionService 
 	private String allFundUri;
 	@Value("${system.http.fund.fundRealTime}")
 	private String fundRealTime;
-
+	@Value("${system.http.juhe.host}")
+	private String juheHost;
+	@Value("${system.http.juhe.suspendPurch.uri}")
+	private String suspendPurchUri;
+	@Value("${system.http.juhe.suspendRedee.uri}")
+	private String suspendRedeeUri;
+	@Value("${system.http.juhe.appKey}")
+	private String appKey;
 	@Override
 	@Transactional(rollbackFor = Exception.class)
 	public void fundCollectionCompanyData(SysTaskModel sysTaskModel) throws Exception {
@@ -73,12 +85,14 @@ public class FundDataCollectionServiceImpl implements FundDataCollectionService 
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void fundCollectionAllFundData(SysTaskModel sysTaskModel) {
 		sysTaskService.progressing(sysTaskModel);
 		fundCollectionAllFundData();
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void retryCollectionAllFundData(SysTaskModel sysTaskModel, int retryCount) {
 		sysTaskModel.setRetryCount(retryCount);
 		sysTaskService.updateById(sysTaskModel);
@@ -86,16 +100,48 @@ public class FundDataCollectionServiceImpl implements FundDataCollectionService 
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void fundCollectionFundRealTimeData(SysTaskModel sysTaskModel) {
 		sysTaskService.progressing(sysTaskModel);
 		fundCollectionFundRealTimeData();
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void retryCollectionFundRealTimeData(SysTaskModel sysTaskModel, int retryCount) {
 		sysTaskModel.setRetryCount(retryCount);
 		sysTaskService.updateById(sysTaskModel);
 		fundCollectionFundRealTimeData();
+	}
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public void fundCollectionFundSuspendPurchData(SysTaskModel sysTaskModel) {
+		sysTaskService.progressing(sysTaskModel);
+		fundCollectionFundSuspendPurchData();
+	}
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public void retryCollectionFundSuspendPurchData(SysTaskModel sysTaskModel, int retryCount) {
+		sysTaskModel.setRetryCount(retryCount);
+		sysTaskService.updateById(sysTaskModel);
+		fundCollectionFundSuspendPurchData();
+	}
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public void fundCollectionFundSuspendRedeeData(SysTaskModel sysTaskModel) {
+		sysTaskService.progressing(sysTaskModel);
+		fundCollectionFundSuspendRedeeData();
+	}
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public void retryCollectionFundSuspendRedeeData(SysTaskModel sysTaskModel, int retryCount) {
+		sysTaskModel.setRetryCount(retryCount);
+		sysTaskService.updateById(sysTaskModel);
+		fundCollectionFundSuspendRedeeData();
 	}
 
 	private void fundCollectionCompanyData(){
@@ -190,6 +236,25 @@ public class FundDataCollectionServiceImpl implements FundDataCollectionService 
 	private void fundCollectionFundRealTimeData(){
 		//1.获取所有基金数据
 		List<FundInfoModel> fundInfoModelList = fundInfoService.list();
+		FundSuspendModel maxBatchNum = fundSuspendService.getOne(new QueryWrapper<FundSuspendModel>().lambda()
+				.orderByDesc(FundSuspendModel::getBatchNum)
+				.eq(FundSuspendModel::getSuspendType, SuspendTypeEnum.PURCH.getCode())
+				.last("limit 1"));
+		List<FundSuspendModel> fundSuspendModelList = fundSuspendService.list(new QueryWrapper<FundSuspendModel>().lambda()
+				.eq(FundSuspendModel::getSuspendType, SuspendTypeEnum.PURCH.getCode())
+				.eq(FundSuspendModel::getBatchNum,maxBatchNum)
+				.eq(FundSuspendModel::getOpenDate,"-"));
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		long now = System.currentTimeMillis();
+		List<String> suspendFundCodes = fundSuspendModelList.stream().filter(t -> {
+			try {
+				return simpleDateFormat.parse(t.getSuspendDate()).getTime() > now;
+			} catch (ParseException e) {
+				log.error(e.getMessage());
+				return false;
+			}
+		}).map(FundSuspendModel::getFundCode).collect(Collectors.toList());
+		fundInfoModelList = fundInfoModelList.stream().filter(t -> !suspendFundCodes.contains(t.getFundCode())).collect(Collectors.toList());
 		//2.获取单个基金实时数据
 		List<FundRealTimeDataModel> fundRealTimeDataModelList = new ArrayList<>(fundInfoModelList.size());
 		for(FundInfoModel fundInfoModel : fundInfoModelList){
@@ -243,5 +308,118 @@ public class FundDataCollectionServiceImpl implements FundDataCollectionService 
 
 	private void dbSubmitFundRealTimeData(List<FundRealTimeDataModel> fundRealTimeDataModelList) {
 		fundRealTimeDataService.saveBatch(fundRealTimeDataModelList);
+	}
+	private void fundCollectionFundSuspendPurchData(){
+		//1.获取暂停申购的基金数据
+		String jsonString = getFundSuspendPurchData();
+		//2.解析数据
+		List<FundSuspendModel> fundSuspendModelList = analysisFundSuspendPurchData(jsonString);
+		//3.入库
+		dbSubmitFundSuspendData(fundSuspendModelList);
+	}
+
+	private String getFundSuspendPurchData() {
+		String url = juheHost + suspendPurchUri + "?key=" + appKey;
+		log.info("url:{}", url);
+		String jsonString = restTemplate.getForObject(url, String.class);
+		log.info("result:{}", jsonString);
+		return jsonString;
+	}
+
+	private List<FundSuspendModel> analysisFundSuspendPurchData(String jsonString) {
+		JSONObject jsonObject = JSON.parseObject(jsonString);
+		if(Objects.equals(jsonObject.getString("resultcode"),"200")){
+			JSONArray jsonArray = jsonObject.getJSONArray("result");
+			if(CollectionUtils.isNotEmpty(jsonArray)){
+				String batchNum = String.valueOf(System.currentTimeMillis());
+				List<FundSuspendModel> fundSuspendModelList = new ArrayList<>();
+				for(Object obj : jsonArray){
+					JSONObject data = (JSONObject)obj;
+					OptionalInt max = data.keySet().stream().mapToInt(Integer::parseInt).max();
+					if(max.isPresent()){
+						FundSuspendModel fundSuspendModel = null;
+						for(int i = 1; i <= max.getAsInt(); ++i){
+							JSONObject fundData = (JSONObject)data.get(String.valueOf(i));
+							fundSuspendModel = new FundSuspendModel();
+							fundSuspendModel.setBatchNum(batchNum);
+							fundSuspendModel.setCreateTime(new Date());
+							fundSuspendModel.setFundCode(fundData.getString("code"));
+							fundSuspendModel.setFundName(fundData.getString("name"));
+							fundSuspendModel.setSuspendDate(fundData.getString("suspenddate"));
+							fundSuspendModel.setOpenDate(fundData.getString("opendate"));
+							fundSuspendModel.setSuspendType(SuspendTypeEnum.PURCH.getCode());
+							fundSuspendModelList.add(fundSuspendModel);
+						}
+					}
+				}
+				return fundSuspendModelList;
+			}else{
+				return Collections.emptyList();
+			}
+		}else{
+			log.error("reason:{}",jsonObject.getString("reason"));
+			return Collections.emptyList();
+		}
+	}
+
+	private void dbSubmitFundSuspendData(List<FundSuspendModel> fundSuspendModelList) {
+		if(CollectionUtils.isNotEmpty(fundSuspendModelList)) {
+			Integer suspendType = fundSuspendModelList.get(0).getSuspendType();
+			fundSuspendService.remove(new UpdateWrapper<FundSuspendModel>().lambda().eq(FundSuspendModel::getSuspendType,suspendType));
+			fundSuspendService.saveBatch(fundSuspendModelList);
+		}
+	}
+
+	private void fundCollectionFundSuspendRedeeData(){
+		//1.获取暂停赎回的基金数据
+		String jsonString = getFundSuspendRedeeData();
+		//2.解析数据
+		List<FundSuspendModel> fundSuspendModelList = analysisFundSuspendRedeeData(jsonString);
+		//3.入库
+		dbSubmitFundSuspendData(fundSuspendModelList);
+	}
+
+	private String getFundSuspendRedeeData() {
+		String url = juheHost + suspendRedeeUri + "?key=" + appKey;
+		log.info("url:{}", url);
+		String jsonString = restTemplate.getForObject(url, String.class);
+		log.info("result:{}", jsonString);
+		return jsonString;
+	}
+
+	private List<FundSuspendModel> analysisFundSuspendRedeeData(String jsonString) {
+		JSONObject jsonObject = JSON.parseObject(jsonString);
+		if(Objects.equals(jsonObject.getString("resultcode"),"200")){
+			JSONArray jsonArray = jsonObject.getJSONArray("result");
+			if(CollectionUtils.isNotEmpty(jsonArray)){
+				String batchNum = String.valueOf(System.currentTimeMillis());
+				List<FundSuspendModel> fundSuspendModelList = new ArrayList<>();
+				for(Object obj : jsonArray){
+					JSONObject data = (JSONObject)obj;
+					OptionalInt max = data.keySet().stream().mapToInt(Integer::parseInt).max();
+					if(max.isPresent()){
+						FundSuspendModel fundSuspendModel = null;
+						for(int i = 1; i <= max.getAsInt(); ++i){
+							JSONObject fundData = (JSONObject)data.get(String.valueOf(i));
+							fundSuspendModel = new FundSuspendModel();
+							fundSuspendModel.setBatchNum(batchNum);
+							fundSuspendModel.setCreateTime(new Date());
+							fundSuspendModel.setFundCode(fundData.getString("code"));
+							fundSuspendModel.setFundName(fundData.getString("name"));
+							fundSuspendModel.setSuspendDate(fundData.getString("suspenddate"));
+							fundSuspendModel.setOpenDate(fundData.getString("opendate"));
+							fundSuspendModel.setSuspendType(SuspendTypeEnum.REDEE.getCode());
+							fundSuspendModelList.add(fundSuspendModel);
+						}
+					}
+				}
+				return fundSuspendModelList;
+			}else{
+				return Collections.emptyList();
+			}
+		}else{
+			log.error("reason:{}",jsonObject.getString("reason"));
+			return Collections.emptyList();
+		}
 	}
 }
